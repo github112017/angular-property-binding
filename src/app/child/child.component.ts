9dc81cd4e162f25ee9bf3ce34eab3afcb8032710
Vivekanand P V <vivekanandpv@outlook.com>
2019-09-08 18:33:00 +0530

    Init

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnChanges {
   @Input() childProperty;
 
-  constructor() {}
+  constructor() {
+    console.log('Constructor called');
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('ngOnInit called');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges Called with:', changes);
+  }
 }
